c738f41707dbb29ba3b95a576ee0f34d0f8d781f
Swe <864876906@qq.com>
2018-12-09 14:06:55 +0800

    22

--- src/Hell.java
+++ src/Hell.java
@@ -2,5 +2,6 @@ public class Hell {
     public static void  main(String[] args){
         System.out.println("this if a ch");
         System.out.println("dfsdfsd");
+        System.out.println("fsafsdfa");
     }
 }
